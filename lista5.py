966f9a6eee4aba7fd96836d1bce2d88c996f9ce0
Your Name <you@example.com>
2024-09-05 12:12:58 -0300

    lista 5

---
+++ lista5.py
@@ -0,0 +1,26 @@
+#Ex1
+x = 2
+y = 5
+if y > 8:
+    y = y*2
+else:
+    x = x * 2
+print(x+y)
+
+#Ex2
+lista=[]
+for i in range(9):
+    if i != 3:
+        for j in range(6):
+            lista.append('python é legal')
+print(len(lista))
+lista = []
+
+#Ex3
+for i in range (1068, 3628, 2):
+    if i % 7 == 0:
+        lista.append('python é legal')
+print(len(lista))
+lista=[]
+
+#Ex4
